52ffe04986bc320e4a5b5ee235a91b31865cad74
badmusa4 <shafiakinropo@gmail.com>
2024-05-31 13:50:05 +0100

    Update WelcomeEmail.php

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -8,6 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use App\Models\User;
 
 class WelcomeEmail extends Mailable
 {
